7115833bd5d26ac9b6f0e9b56484692335947a15
PureVessel99 <maxiwasi1999@gmail.com>
2023-06-03 22:11:49 -0300

    initial commit

---
+++ contador/src/assets/imagenes.js
@@ -0,0 +1,7 @@
+import moto from './img/moto';
+import Phase_2_HD from './img/Phase_2_HD'
+
+export default {
+    "img1" :moto,
+    "img2" :Phase_2_HD
+}
---
+++ contador/src/firebase-config.js
@@ -0,0 +1,10 @@
+export default {
+
+    apiKey: "AIzaSyCzChF0c6kYRUnSz5NslVhA00W4Z2n_iLQ",
+    authDomain: "contador-539cd.firebaseapp.com",
+    projectId: "contador-539cd",
+    storageBucket: "contador-539cd.appspot.com",
+    messagingSenderId: "635444399983",
+    appId: "1:635444399983:web:5cda7a0f2fe09ea86a629a"
+
+}
